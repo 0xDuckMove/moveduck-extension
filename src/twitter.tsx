af981923176f4d54b9a87ebb1307460bfb3678c7
tung-lee <lekhacthanhtung.it@gmail.com>
2024-08-19 22:44:09 +0700

    feat: update render component

--- src/twitter.tsx
+++ src/twitter.tsx
@@ -11,21 +11,9 @@ export function setupTwitterObserver(
                 if (node.nodeType !== Node.ELEMENT_NODE) {
                     return;
                 }
-                if (node.nodeType === Node.ELEMENT_NODE) {
-                    const element = node as Element;
-                    const anchors = [
-                        ...(element.tagName.toLowerCase() === 'a' ? [element] : []),
-                        ...element.querySelectorAll('a')
-                    ];
-
-                    for (const anchor of anchors) {
-                        const href = anchor.getAttribute('href');
-                        if (href && href.toLowerCase().startsWith('http')) {
-                            console.log('Found anchor element with http link');
-                            handleNewNode(anchor).catch(noop);
-                        }
-                    }
-                }
+                handleNewNode(
+                    node as Element,
+                ).catch(noop);
             }
         }
     });
@@ -40,8 +28,38 @@ async function handleNewNode(
     node: Element,
 ) {
     const element = node as Element;
+    if (!element || element.localName !== 'div') {
+        return;
+    }
 
-    element.replaceChildren(createAction());
+    let anchor;
+
+    const linkPreview = findLinkPreview(element);
+
+    let container = findContainerInTweet(
+        linkPreview?.card ?? element,
+        Boolean(linkPreview),
+    );
+    if (linkPreview) {
+        anchor = linkPreview.anchor;
+        container && container.remove();
+        container = linkPreview.card.parentElement as HTMLElement;
+    } else {
+        if (container) {
+            return;
+        }
+        const link = findLastLinkInText(element);
+        if (link) {
+            anchor = link.anchor;
+            container = getContainerForLink(link.tweetText);
+        }
+    }
+
+    if (!anchor || !container) return;
+
+    addMargin(container).replaceChildren(
+        createAction(),
+    );
 }
 
 
@@ -58,4 +76,72 @@ function createAction() {
         </div>,
     );
     return container;
+}
+
+function findElementByTestId(element: Element, testId: string) {
+    if (element.attributes.getNamedItem('data-testid')?.value === testId) {
+        return element;
+    }
+    return element.querySelector(`[data-testid="${testId}"]`);
+}
+
+function findContainerInTweet(element: Element, searchUp?: boolean) {
+    const message = searchUp
+        ? (element.closest(`[data-testid="tweet"]`) ??
+            element.closest(`[data-testid="messageEntry"]`))
+        : (findElementByTestId(element, 'tweet') ??
+            findElementByTestId(element, 'messageEntry'));
+
+    if (message) {
+        return message.querySelector('.dialect-wrapper') as HTMLElement;
+    }
+    return null;
+}
+
+function findLinkPreview(element: Element) {
+    const card = findElementByTestId(element, 'card.wrapper');
+    if (!card) {
+        return null;
+    }
+
+    const anchor = card.children[0]?.children[0] as HTMLAnchorElement;
+
+    return anchor ? { anchor, card } : null;
+}
+
+function findLastLinkInText(element: Element) {
+    const tweetText = findElementByTestId(element, 'tweetText');
+    if (!tweetText) {
+        return null;
+    }
+
+    const links = tweetText.getElementsByTagName('a');
+    if (links.length > 0) {
+        const anchor = links[links.length - 1] as HTMLAnchorElement;
+        return { anchor, tweetText };
+    }
+    return null;
+}
+
+function getContainerForLink(tweetText: Element) {
+    const root = document.createElement('div');
+    root.className = 'dialect-wrapper';
+    const dm = tweetText.closest(`[data-testid="messageEntry"]`);
+    if (dm) {
+        root.classList.add('dialect-dm');
+        tweetText.parentElement?.parentElement?.prepend(root);
+    } else {
+        tweetText.parentElement?.append(root);
+    }
+    return root;
+}
+
+function addMargin(element: HTMLElement) {
+    if (element && element.classList.contains('dialect-wrapper')) {
+        element.style.marginTop = '12px';
+        if (element.classList.contains('dialect-dm')) {
+            element.style.marginBottom = '8px';
+        }
+    }
+    return element;
 }
