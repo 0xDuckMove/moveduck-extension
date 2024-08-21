e06f5912e681499fc04d468396244112befbb4f7
lou1s vuong <lou1svuong.dev@gmail.com>
2024-08-21 08:50:29 +0700

    chore: Update Twitter observer to handle new node elements

--- src/twitter.tsx
+++ src/twitter.tsx
@@ -1,29 +1,110 @@
 import { createRoot } from 'react-dom/client';
 import { noop } from './utils/constants';
 import ActionContainer from './popup/components/ActionContainer';
+import { StylePreset } from './popup/components/ActionLayout';
 import '@dialectlabs/blinks/index.css';
+// override the default styles
+import './index.css';
+import { ActionAdapter } from './api/ActionConfig';
+import { ActionCallbacksConfig, ObserverOptions } from '@dialectlabs/blinks';
+import { checkSecurity, SecurityLevel } from './shared';
+import {
+  ActionsRegistry,
+  getExtendedActionState,
+  getExtendedInterstitialState,
+  getExtendedWebsiteState,
+} from './api/ActionsRegistry';
+import { isInterstitial } from './utils/interstitial-url';
+import { proxify } from './utils/proxify';
+import { ActionsJsonConfig, ActionsURLMapper } from './utils/url-mapper';
 
-export function setupTwitterObserver() {
-  const observer = new MutationObserver((mutations) => {
-    for (let i = 0; i < mutations.length; i++) {
-      const mutation = mutations[i];
-      for (let j = 0; j < mutation.addedNodes.length; j++) {
-        const node = mutation.addedNodes[j];
-        if (node.nodeType !== Node.ELEMENT_NODE) {
-          return;
-        }
-        handleNewNode(node as Element).catch(noop);
-      }
-    }
-  });
+type ObserverSecurityLevel = SecurityLevel;
 
-  const twitterReactRoot = document.getElementById('react-root')!;
-
-  observer.observe(twitterReactRoot, { childList: true, subtree: true });
+interface NormalizedObserverOptions {
+  securityLevel: Record<
+    'websites' | 'interstitials' | 'actions',
+    ObserverSecurityLevel
+  >;
 }
 
-async function handleNewNode(node: Element) {
+const DEFAULT_OPTIONS: ObserverOptions = {
+  securityLevel: 'only-trusted',
+};
+
+const normalizeOptions = (
+  options: Partial<ObserverOptions>,
+): NormalizedObserverOptions => {
+  return {
+    ...DEFAULT_OPTIONS,
+    ...options,
+    securityLevel: (() => {
+      if (!options.securityLevel) {
+        return {
+          websites: DEFAULT_OPTIONS.securityLevel as ObserverSecurityLevel,
+          interstitials: DEFAULT_OPTIONS.securityLevel as ObserverSecurityLevel,
+          actions: DEFAULT_OPTIONS.securityLevel as ObserverSecurityLevel,
+        };
+      }
+
+      if (typeof options.securityLevel === 'string') {
+        return {
+          websites: options.securityLevel,
+          interstitials: options.securityLevel,
+          actions: options.securityLevel,
+        };
+      }
+
+      return options.securityLevel;
+    })(),
+  };
+};
+
+export function setupTwitterObserver(
+  config: ActionAdapter,
+  callbacks: Partial<ActionCallbacksConfig> = {},
+  options: Partial<ObserverOptions> = DEFAULT_OPTIONS,
+) {
+  const mergedOptions = normalizeOptions(options);
+  const twitterReactRoot = document.getElementById('react-root')!;
+
+  const refreshRegistry = async () => {
+    return ActionsRegistry.getInstance().init();
+  };
+
+  // if we don't have the registry, then we don't show anything
+  refreshRegistry().then(() => {
+    // entrypoint
+    const observer = new MutationObserver((mutations) => {
+      // it's fast to iterate like this
+      for (let i = 0; i < mutations.length; i++) {
+        const mutation = mutations[i];
+        for (let j = 0; j < mutation.addedNodes.length; j++) {
+          const node = mutation.addedNodes[j];
+          if (node.nodeType !== Node.ELEMENT_NODE) {
+            return;
+          }
+          handleNewNode(
+            node as Element,
+            config,
+            callbacks,
+            mergedOptions,
+          ).catch(noop);
+        }
+      }
+    });
+
+    observer.observe(twitterReactRoot, { childList: true, subtree: true });
+  });
+}
+
+async function handleNewNode(
+  node: Element,
+  config: ActionAdapter,
+  callbacks: Partial<ActionCallbacksConfig>,
+  options: NormalizedObserverOptions,
+) {
   const element = node as Element;
+  // first quick filtration
   if (!element || element.localName !== 'div') {
     return;
   }
@@ -53,6 +134,49 @@ async function handleNewNode(node: Element) {
 
   if (!anchor || !container) return;
 
+  const shortenedUrl = anchor.href;
+  const actionUrl = await resolveTwitterShortenedUrl(shortenedUrl);
+  const interstitialData = isInterstitial(actionUrl);
+
+  let actionApiUrl: string | null;
+  if (interstitialData.isInterstitial) {
+    const interstitialState = getExtendedInterstitialState(
+      actionUrl.toString(),
+    );
+
+    if (
+      !checkSecurity(interstitialState, options.securityLevel.interstitials)
+    ) {
+      return;
+    }
+
+    actionApiUrl = interstitialData.decodedActionUrl;
+  } else {
+    const websiteState = getExtendedWebsiteState(actionUrl.toString());
+
+    if (!checkSecurity(websiteState, options.securityLevel.websites)) {
+      return;
+    }
+
+    // const actionsJsonUrl = actionUrl.origin + '/actions.json';
+    // const actionsJson = await fetch(proxify(actionsJsonUrl)).then(
+    //   (res) => res.json() as Promise<ActionsJsonConfig>,
+    // );
+
+    // const actionsUrlMapper = new ActionsURLMapper(actionsJson);
+
+    // actionApiUrl = actionsUrlMapper.mapUrl(actionUrl);
+  }
+
+  // const state = actionApiUrl ? getExtendedActionState(actionApiUrl) : null;
+  // if (
+  //   !actionApiUrl ||
+  //   !state ||
+  //   !checkSecurity(state, options.securityLevel.actions)
+  // ) {
+  //   return;
+  // }
+
   addMargin(container).replaceChildren(createAction());
 }
 
@@ -64,10 +188,30 @@ function createAction() {
 
   actionRoot.render(
     <div onClick={(e) => e.stopPropagation()}>
-      <ActionContainer />
+      <ActionContainer stylePreset={resolveXStylePreset()} />
     </div>,
   );
   return container;
+}
+
+const resolveXStylePreset = (): StylePreset => {
+  const colorScheme = document.querySelector('html')?.style.colorScheme;
+
+  if (colorScheme) {
+    return colorScheme === 'dark' ? 'x-dark' : 'x-light';
+  }
+
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  return prefersDark ? 'x-dark' : 'x-light';
+};
+
+async function resolveTwitterShortenedUrl(shortenedUrl: string): Promise<URL> {
+  const res = await fetch(shortenedUrl);
+  const html = await res.text();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+  const actionUrl = doc.querySelector('title')?.textContent;
+  return new URL(actionUrl!);
 }
 
 function findElementByTestId(element: Element, testId: string) {
