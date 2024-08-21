75982b7442f2f496a5b906d1a00c688714b22779
lou1s vuong <lou1svuong.dev@gmail.com>
2024-08-21 08:51:33 +0700

    chore: Update header logo and link to ActionX website

--- src/popup/components/Header.tsx
+++ src/popup/components/Header.tsx
@@ -1,15 +1,14 @@
-import DialectLogo from '../assets/DialectLogo';
 import CircleInfoIcon from '../icons/CircleInfoIcon';
 
 export const Header = () => {
   return (
     <div className="flex justify-center items-center py-[18px]">
-      <DialectLogo />
+      <h1 className="font-bold text-xl">ActionX</h1>
       <button
         className="absolute right-4 top-4 text-icon-secondary hover:text-icon-primary"
         onClick={() =>
           chrome.tabs.create({
-            url: 'https://www.dialect.to/',
+            url: 'https://www.actionX.com/',
           })
         }
       >
