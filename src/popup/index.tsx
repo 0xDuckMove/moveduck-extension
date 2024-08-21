0a6de495594d39328401d73539d51b82535510ce
lou1s vuong <lou1svuong.dev@gmail.com>
2024-08-21 08:51:08 +0700

    chore: Update popup CSS import path

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './popup.css';
 import { Popup } from './Popup';
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
