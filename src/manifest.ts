383df4827266e2a24e20669de2e3a91426e761f5
lou1s vuong <lou1svuong.dev@gmail.com>
2024-08-22 12:42:28 +0700

    refactor: Update manifest.ts to use new logo.png for default_icon

--- src/manifest.ts
+++ src/manifest.ts
@@ -9,15 +9,9 @@ export default defineManifest({
   description: packageData.description,
   version: packageData.version,
   manifest_version: 3,
-  icons: {
-    16: 'img/logo-16.png',
-    32: 'img/logo-32.png',
-    48: 'img/logo-48.png',
-    128: 'img/logo-128.png',
-  },
   action: {
     default_popup: 'popup.html',
-    default_icon: 'img/logo-48.png',
+    default_icon: 'img/logo.png',
   },
   background: {
     service_worker: 'src/background.ts',
@@ -25,18 +19,17 @@ export default defineManifest({
   },
   content_scripts: [
     {
-      matches: ['https://twitter.com/*', 'https://x.com/*', 'https://pro.x.com/*'],
+      matches: [
+        'https://twitter.com/*',
+        'https://x.com/*',
+        'https://pro.x.com/*',
+      ],
       js: ['src/contentScript.ts'],
     },
   ],
   web_accessible_resources: [
     {
-      resources: [
-        'img/logo-16.png',
-        'img/logo-32.png',
-        'img/logo-48.png',
-        'img/logo-128.png',
-      ],
+      resources: ['img/logo.png'],
       matches: [],
     },
   ],
